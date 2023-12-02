621fe3a8b082decb222eb07bb118363ba7a43a0e
Fanny Hoberg <fannyh.92@hotmail.com>
2023-12-02 14:18:15 +0100

    Shuffle array OK, photo OK, text for incorrect answers OK, text for correct answer NOT OK

--- script.js
+++ script.js
@@ -14,48 +14,67 @@ startButtonEl.addEventListener("click", () => {
 
     section1El.classList.remove("hide");
     // section2El.classList.remove("hide");
-  }, 2000);
+  }, 1000);
 });
 
-//rendera ut game initial setup
-const nameEl = document.querySelector("p");
+//SHUFFLING
+
+// Fisher-Yates algorithm for array shuffling
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
+const shuffledStudents = [...students]; // clone `students` array
+shuffleArray(shuffledStudents); // shuffle the `shuffledStudents` array
+
+console.log(students);
+console.log(shuffledStudents);
+
+// RENDERA ut game initial setup
+
+const nameEl = document.getElementsByClassName("namesFigure");
+// const box1 = document.getElementById("box-1");
 const imageEl = document.getElementById("imagesFigure");
 
 const renderGameSetup = () => {
   // Slumpa array students (Fisher Yates shuffle)
 
   // Välj ut x antal från students array - lägg i variabel total
-  const newArray10 = students.slice(0, 10);
-  console.log("Det är här newArray10: ", newArray10);
+  const newArray10 = shuffledStudents.slice(0, 10);
+  // console.log("Det är här newArray10: ", newArray10);
 
   // Från total ska 1 object tas ut som rätt svar (id === name === image) och lägg i variabel match
   const match = newArray10.pop();
   console.log("Det är här match: ", match);
 
   // Från total ska 3 name tas ut och lägg i variabel unmatch
-  console.log("Det är här newArray10 efter match tagits bort: ", newArray10);
   const unmatch = newArray10.slice(0, 3);
   console.log("Det är här unmatch: ", unmatch);
 
   // Rendera ut match.image på bild div
-
   imageEl.innerHTML = "";
-
   const imgElement = document.createElement("img");
-
   imgElement.src = match.image;
-
   imageEl.appendChild(imgElement);
 
-  // Rendera ut match.name och unmatch.name på alla namn div
+  // Rendera ut match.name
+  // nameEl.innerText = match.name;
 
-  // nameEl.innerText = "";
-  // const nameElement = document.createElement("p");
-  // nameElement.innerText = match.name;
-  // nameEl.appendChild(nameElement);
+  // Rendera ut unmatch.name på alla namn div
+  for (let i = 0; i < nameEl.length && i < unmatch.length; i++) {
+    let unmatchPerson = unmatch[i];
+    let boxForName = nameEl[i];
+    let nameText = document.createElement("div");
+    nameText.innerHTML = `<p>${unmatchPerson.name}</p>`;
+    boxForName.appendChild(nameText);
+  }
 
-  nameEl.innerText = unmatch.map((student) => {
-    return student.name;
-  });
+  //slumpa alla boxar på dess id
 };
+
 renderGameSetup();
