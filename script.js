9a684e0494f286ca130e854d29aed27b877967a6
Fanny Hoberg <fannyh.92@hotmail.com>
2023-12-06 20:02:15 +0100

    Feedback on correct vs incorrect answer OK, Improved CSS style

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const section2El = document.querySelector(".section2");
 const namesFrameEl = document.querySelector(".namesFrame");
 const sectionResultEl = document.querySelector(".sectionResult");
 const guessesEl = document.querySelector("#guesses");
+const feedbackEl = document.querySelector("#feedback");
 
 let newArray;
 let match;
 let unmatch;
 let total;
+let restArray;
 
 // Fisher-Yates algorithm for array shuffling
 const shuffleArray = (array) => {
@@ -34,56 +36,82 @@ startButtonEl.addEventListener("click", (e) => {
   }, 1000);
   if (e.target.tagName === "BUTTON") {
     if (e.target.id === "startButton1") {
-      console.log("Du klickade på knapp 1: ", e);
       newArray = shuffledStudents.slice(0, 10);
+      restArray = shuffledStudents.slice(11, 17);
       shuffleArray(newArray);
       total = 10;
       console.log("Det är här newArray: ", newArray);
       renderGameSetup();
     } else if (e.target.id === "startButton2") {
-      console.log("Du klickade på knapp 2: ", e);
       newArray = shuffledStudents.slice(0, 20);
+      restArray = shuffledStudents.slice(21, 27);
       shuffleArray(newArray);
       total = 20;
       console.log("Det är här newArray: ", newArray);
       renderGameSetup();
     } else {
-      console.log("Du klickade på knapp 3: ", e);
       newArray = shuffledStudents;
+      restArray = shuffledStudents.slice(0, 20);
       shuffleArray(newArray);
-      total = students.length;
+      total = shuffledStudents.length;
       console.log("Det är här newArray: ", newArray);
       renderGameSetup();
     }
   } else {
-    //ska lösa någon snygg escape/return här
+    //vad händer när man klickar utanför?
   }
 });
 
+const correctGuessColor = () => {
+  feedbackEl.className = "greenFrame";
+  feedbackEl.innerHTML = `<p>Du gissade rätt!</p>`;
+};
+
+const incorrectGuessColor = () => {
+  feedbackEl.className = "redFrame";
+  feedbackEl.innerHTML = `<p>Du gissade fel!</p>`;
+};
+
+const removeFeedback = () => {
+  feedbackEl.className = "";
+  feedbackEl.innerHTML = `<p></p>`;
+};
+
 let correctGuesses = 0;
+let clickCount = 0;
+
 //Clickevent för knappar med namn
 namesFrameEl.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
     if (e.target.innerText === match.name || e.target.tagName === "P") {
       console.log("Du klickade på RÄTT namn; ", e);
-      namesFrameEl.classList.add("greenFrame");
+      correctGuessColor();
+      // namesFrameEl.classList.add("greenFrame");
       correctGuesses++;
     } else {
       console.log("Du klickade på FEL namn; ", e);
-      namesFrameEl.classList.add("redFrame");
+      incorrectGuessColor();
     }
   }
-  //ta bort match från array
-  newArray = newArray.filter(function (item) {
+
+  newArray = newArray.filter((item) => {
     return item !== match;
   });
-  console.log(
-    "Detta är newArray efter match blivit borttaget; ",
-    newArray.length
-  );
-  //shuffla listan igen innan render
+
+  clickCount++;
+
+  if (clickCount === total) {
+    section1El.classList.add("hide");
+    section2El.classList.add("hide");
+    sectionResultEl.classList.remove("hide");
+    guessesEl.innerText = `${correctGuesses}/ ${total}`;
+  }
+
   shuffleArray(newArray);
-  renderGameSetup();
+
+  setTimeout(() => {
+    renderGameSetup();
+  }, 1000);
 });
 
 // RENDERA ut game setup
@@ -91,75 +119,56 @@ const nameEl = document.getElementsByClassName("namesBox");
 const imageEl = document.getElementById("imagesFigure");
 
 const renderGameSetup = () => {
-  console.log("Det är här newArray från render: ", newArray);
   matchArray = newArray.slice(0, 1);
   match = matchArray[0];
-  // match = newArray.pop();
-  console.log("Det är här match från render: ", match);
   unmatch = newArray.slice(1, 4);
+  console.log("Det är här match från render: ", match);
   console.log("Det är här unmatch från render: ", unmatch);
+  console.log("Det är här newArray från render: ", newArray);
+  console.log("Det är här restArray från render: ", restArray);
+
+  //ta bort grön/röd färg innan nästa omgång
+  removeFeedback();
 
   // Rendera ut match.image på bild div
   imageEl.innerHTML = "";
-  const imgElement = document.createElement("img");
-  imgElement.src = match.image;
-  imageEl.appendChild(imgElement);
+  const imageBox = document.createElement("img");
+  imageBox.setAttribute("src", match.image);
+  imageEl.append(imageBox);
 
-  // // Rendera ut match.name på random knapp
+  // Rendera ut namen på random knappar
   const randomLocation = (max = 4) => {
     return Math.ceil(Math.random() * max);
   };
 
-  function renderCorrectName() {
-    let randomIndex = randomLocation();
-    let randomBoxId = "box-" + randomIndex;
-    let selectedBox = document.getElementById(randomBoxId);
-    selectedBox.innerHTML = `<p>${match.name}</p>`;
+  function renderNameToButton() {
+    for (let i = 1; i <= 4; i++) {
+      let buttonBoxId = "box-" + i;
+      let button = document.getElementById(buttonBoxId);
+      button.innerHTML = "";
+    }
 
-    renderIncorrectNames();
-  }
+    let buttonNames = unmatch.map((person) => person.name);
+    buttonNames.push(match.name);
+    shuffleArray(buttonNames);
 
-  function renderIncorrectNames() {
-    for (let i = 0; i < nameEl.length && i < unmatch.length; i++) {
-      let unmatchPerson = unmatch[i];
-      let button = nameEl[i];
-      if (button.innerText.trim() === "") {
-        button.innerHTML = `<p>${unmatchPerson.name}</p>`;
+    for (let i = 0; i < buttonNames.length; i++) {
+      let buttonBoxId = "box-" + (i + 1);
+      let button = document.getElementById(buttonBoxId);
+      button.innerHTML = `<p>${buttonNames[i]}</p>`;
+    }
+
+    for (let i = 1; i <= 4; i++) {
+      let buttonBoxId = "box-" + i;
+      let button = document.getElementById(buttonBoxId);
+
+      if (button.innerHTML === "") {
+        if (restArray.length > 0) {
+          let restObject = restArray.shift();
+          button.innerHTML = `<p>${restObject.name}</p>`;
+        }
       }
     }
   }
-  renderCorrectName();
-
-  // Rendera ut unmatch.name på lediga knappar
-  // for (let i = 0; i < nameEl.length && i < unmatch.length; i++) {
-  //   let unmatchPerson = unmatch[i];
-  //   let boxForName = nameEl[i];
-  //   nameEl.innerHTML = "";
-
-  //   if (boxForName.textContent === "") {
-  //     let nameText = document.createElement("p");
-  //     nameText.innerText = `${unmatchPerson.name}`;
-  //     boxForName.appendChild(nameText);
-  //   }
-  // }
-  // // Rendera ut match.name
-  // for (let i = 0; i < nameEl.length; i++) {
-  //   let currentBox = nameEl[i];
-  //   if (currentBox.textContent === "") {
-  //     let boxForName = nameEl[i];
-  //     let nameText = document.createElement("p");
-  //     nameText.innerText = `${match.name}`;
-  //     boxForName.appendChild(nameText);
-  //   }
-  // }
-
-  //spelet slut - visa resultat sida med score
-  if (newArray.length === 0) {
-    section1El.classList.add("hide");
-    section2El.classList.add("hide");
-    sectionResultEl.classList.remove("hide");
-    guessesEl.innerText = `${correctGuesses}/ ${total}`;
-  }
+  renderNameToButton();
 };
-
-// renderGameSetup();
