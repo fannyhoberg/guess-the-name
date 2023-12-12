af18815121e06cb4c85832869d420609e943ecc3
Fanny Hoberg <fannyh.92@hotmail.com>
2023-12-12 12:12:01 +0100

    Fixed problem with double names

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const sectionStartEl = document.querySelector(".sectionStart");
-// const startButtonEl = document.querySelector(".startButton");
 const startButtonsEl = document.querySelectorAll(".startButtons");
 const section1El = document.querySelector(".section1");
 const section2El = document.querySelector(".section2");
-// const namesFrameEl = document.querySelector(".namesFrame");
 const guessButtons = document.querySelectorAll(".namesBox");
 const sectionResultEl = document.querySelector(".sectionResult");
 const guessesEl = document.querySelector("#guesses");
 const feedbackEl = document.querySelector("#feedback");
+const nameEl = document.getElementsByClassName("namesBox");
+const imageEl = document.getElementById("imagesFigure");
 
 let newArray;
 let match;
 let unmatch;
 let total;
-let restArray;
+let imagesArray;
+let namesArray;
+
+let removedStudents = [];
 
 // Fisher-Yates algorithm for array shuffling
 const shuffleArray = (array) => {
@@ -25,53 +28,77 @@ const shuffleArray = (array) => {
   }
 };
 
-//skapa en kopia av students och shuffla
+// Skapa en kopia av students och shuffla
 const shuffledStudents = [...students];
 shuffleArray(shuffledStudents);
 
-// eventlistener för klick på startsida
+// Eventlistener för klick på startsida
 startButtonsEl.forEach((button) => {
   button.addEventListener("click", (e) => {
     sectionStartEl.classList.add("hide");
+
     setTimeout(() => {
       section1El.classList.remove("hide");
       section2El.classList.remove("hide");
     }, 1000);
+
     if (e.target.tagName === "BUTTON") {
       if (e.target.id === "startButton1") {
         newArray = shuffledStudents.slice(0, 10);
-        restArray = shuffledStudents.slice(11, 17);
         shuffleArray(newArray);
         total = 10;
-        console.log("Det är här newArray: ", newArray);
+
+        imagesArray = newArray.filter((object, index) => index < total);
+        shuffleArray(imagesArray);
+
+        namesArray = newArray
+          .filter((object, index) => index < total)
+          .map((object) => ({ id: object.id, name: object.name }));
+        shuffleArray(namesArray);
+
         renderGameSetup();
       } else if (e.target.id === "startButton2") {
         newArray = shuffledStudents.slice(0, 20);
-        restArray = shuffledStudents.slice(21, 27);
         shuffleArray(newArray);
         total = 20;
-        console.log("Det är här newArray: ", newArray);
+
+        imagesArray = newArray.filter((object, index) => index < total);
+        shuffleArray(imagesArray);
+
+        namesArray = newArray
+          .filter((object, index) => index < total)
+          .map((object) => ({ id: object.id, name: object.name }));
+        shuffleArray(namesArray);
+
         renderGameSetup();
       } else {
         newArray = shuffledStudents;
-        restArray = shuffledStudents.slice(0, 40);
         shuffleArray(newArray);
         total = shuffledStudents.length;
-        console.log("Det är här newArray: ", newArray);
+
+        imagesArray = newArray.filter((object, index) => index < total);
+        shuffleArray(imagesArray);
+
+        namesArray = newArray
+          .filter((object, index) => index < total)
+          .map((object) => ({ id: object.id, name: object.name }));
+        shuffleArray(namesArray);
+
         renderGameSetup();
       }
     }
   });
 });
 
-const correctGuessColor = () => {
+// Feedback
+const correctGuess = () => {
   feedbackEl.className = "correctGuess";
-  feedbackEl.innerHTML = `<p>Du gissade rätt!</p>`;
+  feedbackEl.innerHTML = `<p><strong>Du gissade rätt!</strong></p>`;
 };
 
-const incorrectGuessColor = () => {
+const incorrectGuess = () => {
   feedbackEl.className = "wrongGuess";
-  feedbackEl.innerHTML = `<p>Du gissade fel!</p>`;
+  feedbackEl.innerHTML = `<p><strong>Du gissade fel!</strong></p>`;
 };
 
 const removeFeedback = () => {
@@ -79,65 +106,14 @@ const removeFeedback = () => {
   feedbackEl.innerHTML = `<p></p>`;
 };
 
-let correctGuesses = 0;
-let clickCount = 0;
-
-//Clickevent för knappar med namn
-
-guessButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (e.target.tagName === "BUTTON" || e.target.tagName === "P") {
-      if (e.target.innerText === match.name) {
-        console.log("Du klickade på RÄTT namn; ", e);
-        correctGuessColor();
-        correctGuesses++;
-      } else {
-        console.log("Du klickade på FEL namn; ", e);
-        incorrectGuessColor();
-      }
-    }
-
-    newArray = newArray.filter((item) => {
-      return item !== match;
-    });
-
-    clickCount++;
-    console.log("Antal klick: ", clickCount);
-
-    if (clickCount === total) {
-      setTimeout(() => {
-        section1El.classList.add("hide");
-        section2El.classList.add("hide");
-        sectionResultEl.classList.remove("hide");
-        guessesEl.innerText = `${correctGuesses} / ${total}`;
-      }, 1000);
-    }
-
-    shuffleArray(newArray);
-
-    if (clickCount < total) {
-      setTimeout(() => {
-        renderGameSetup();
-      }, 1000);
-    }
-  });
-});
-
-const nameEl = document.getElementsByClassName("namesBox");
-const imageEl = document.getElementById("imagesFigure");
-
-// rendera ut game setup
+// rendera ut game
 const renderGameSetup = () => {
-  //ta bort feedback innan nästa omgång
   removeFeedback();
 
-  matchArray = newArray.slice(0, 1);
+  // Ta ut 1 match (rätt svar) och 3 unmatch (fel svar)
+  matchArray = imagesArray.slice(0, 1);
   match = matchArray[0];
-  unmatch = newArray.slice(1, 4);
-  console.log("Det är här match från render: ", match);
-  console.log("Det är här unmatch från render: ", unmatch);
-  console.log("Det är här newArray från render: ", newArray);
-  console.log("Det är här restArray från render: ", restArray);
+  unmatch = namesArray.slice(1, 4);
 
   // rendera ut image
   imageEl.innerHTML = "";
@@ -146,19 +122,21 @@ const renderGameSetup = () => {
   imageEl.append(imageBox);
 
   // rendera ut namn på knappar
-
-  //OBS behöver detta ligga inuti en function???
   const renderNameToButton = () => {
-    for (let i = 1; i <= 4; i++) {
-      let buttonBoxId = i;
-      let button = document.getElementById(buttonBoxId);
-      button.innerHTML = "";
-    }
-
+    const max = 4;
     let buttonNames = unmatch.map((person) => {
-      return person.name;
+      if (person.name !== match.name) {
+        return person.name;
+      } else {
+        const alternativeNames = namesArray
+          .filter((p) => p.name !== match.name)
+          .map((p) => p.name);
+        return alternativeNames.length > 0 ? alternativeNames[0] : p.name;
+      }
     });
+    buttonNames = buttonNames.flat();
     buttonNames.push(match.name);
+    buttonNames = buttonNames.slice(0, max);
     shuffleArray(buttonNames);
 
     for (let i = 0; i < buttonNames.length; i++) {
@@ -166,19 +144,46 @@ const renderGameSetup = () => {
       let button = document.getElementById(buttonBoxId);
       button.innerHTML = `<p>${buttonNames[i]}</p>`;
     }
-
-    for (let i = 1; i <= 4; i++) {
-      let buttonBoxId = i;
-      let button = document.getElementById(buttonBoxId);
-
-      if (button.innerHTML === "") {
-        if (restArray.length > 0) {
-          let restObject = restArray.shift();
-          console.log("detta är restobject name ", restObject.name);
-          button.innerHTML = `<p>${restObject.name}</p>`;
-        }
-      }
-    }
   };
   renderNameToButton();
 };
+
+let correctGuesses = 0;
+let clickCount = 0;
+
+//Eventlistener för knappar med namn
+guessButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON" || e.target.tagName === "P") {
+      if (e.target.innerText === match.name) {
+        correctGuess();
+        correctGuesses++;
+      } else {
+        incorrectGuess();
+      }
+    }
+
+    // Ta bort match från imagesArray inför nästa game
+    imagesArray = imagesArray.filter((item) => {
+      return item !== match;
+    });
+
+    // Flytta match till removedStudents array
+    removedStudents.push(match);
+
+    clickCount++;
+
+    if (clickCount === total) {
+      setTimeout(() => {
+        section1El.classList.add("hide");
+        section2El.classList.add("hide");
+        sectionResultEl.classList.remove("hide");
+        guessesEl.innerText = `${correctGuesses} / ${total}`;
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        renderGameSetup();
+      }, 1000);
+    }
+  });
+});
